c30cca0ca53a3a2b6b2359a7f75b0201291cd936
taiki <31148191+taiki0000@users.noreply.github.com>
2019-09-17 14:49:53 +0900

    change 仮完了（ ad39da45d2651bf2c54207b9b4ca7ddd3cb1826a [ad39da4])で消えてしまっていた内容を復元

--- index.php
+++ index.php
@@ -290,7 +290,7 @@
                 <div class="content">
                     <div class="row">
                         <div class="title-area">
-                            <h2>メンバー</h2>
+                            <h2 glot-model="member" >メンバー</h2>
                             <div class="separator separator-danger">✻</div>
                         </div>
                     </div>
@@ -306,9 +306,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/uchiyama.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">内山幸樹</h3>
-                                                    <p class="small-text">株式会社ホットリンク 代表取締役会長</p>
-                                                    <p class="description">東京大学大学院博士課程在学中に日本最初期の検索エンジンの開発に関り、以降２０年以上に渡り、ビッグデータ・AI・検索技術をベースにしたビジネスを多く立ち上げ、世界規模で展開。上場経験後、日本初の全寮制インターナショナル・ハイスクール UWC ISAKの立ち上げに関り、プロジェクトメンバーの柳沢らとの出会いで、自らの経験・知識・人脈が会社経営以外の社会課題解決にも少なからず力を発揮する事に気づき、様々な社会貢献活動を開始</p>
+                                                    <h3 class="title" glot-model="Uchiyama">内山幸樹</h3>
+                                                    <p class="small-text" glot-model="hottolinkK" >株式会社ホットリンク 代表取締役会長</p>
+                                                    <p class="description" glot-model="UchiyamaD" >東京大学大学院博士課程在学中に日本最初期の検索エンジンの開発に関り、以降２０年以上に渡り、ビッグデータ・AI・検索技術をベースにしたビジネスを多く立ち上げ、世界規模で展開。上場経験後、日本初の全寮制インターナショナル・ハイスクール UWC ISAKの立ち上げに関り、プロジェクトメンバーの柳沢らとの出会いで、自らの経験・知識・人脈が会社経営以外の社会課題解決にも少なからず力を発揮する事に気づき、様々な社会貢献活動を開始</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -320,9 +320,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/yanagisawa.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">柳沢正和</h3>
-                                                    <p class="small-text">NPO法人グッドエイジングエールズ</p>
-                                                    <p class="description">LGBT支援の認定NPO法人グッド・エイジング・エールズ所属。学校法人インターナショナル・スクール・オブ・アジア軽井沢（UWC ISAK Japan)評議員、ヒューマン・ライツ・ウォッチ東京委員。work with prideプロジェクトに参画し、企業によるLGBT施策の取り組みを評価する「Pride指標」を立ち上げる。2016年の世界経済フォーラムではLGBTセッションでパネリストを務め、ファイナンシャル・タイムス「世界のLGBTエクゼクティブ100人」にも選出</p>
+                                                    <h3 class="title" glot-model="yanagisawa">柳沢正和</h3>
+                                                    <p class="small-text" glot-model="NPO" >NPO法人グッドエイジングエールズ</p>
+                                                    <p class="description" glot-model="yanagisawaD" >LGBT支援の認定NPO法人グッド・エイジング・エールズ所属。学校法人インターナショナル・スクール・オブ・アジア軽井沢（UWC ISAK Japan)評議員、ヒューマン・ライツ・ウォッチ東京委員。work with prideプロジェクトに参画し、企業によるLGBT施策の取り組みを評価する「Pride指標」を立ち上げる。2016年の世界経済フォーラムではLGBTセッションでパネリストを務め、ファイナンシャル・タイムス「世界のLGBTエクゼクティブ100人」にも選出</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -334,9 +334,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/ishiwata.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">石渡広一郎</h3>
-                                                    <p class="small-text">株式会社ホットリンクCEO特別補佐</p>
-                                                    <p class="description">音楽CD流通会社、芸能事務所を経て弁理士へ。独立後、縁あって株式会社ホットリンクの監査役に就任し、現在はCEO特別補佐を務める。中央集権に頼らない本人認証を可能にするブロックチェーン技術に着目し、さらに日本を始め世界でもまだ十分とはいえない多様性に対する不寛容に問題意識を持ち「famiee」の起ち上げから関わる。OneMile商標知的財産事務所。Arts and Lawメンバー。一般社団法人Thinking Entertainments共同代表</p>
+                                                    <h3 class="title" glot-model="ishiwata">石渡広一郎</h3>
+                                                    <p class="small-text" glot-model="hottolinkKK">株式会社ホットリンクCEO特別補佐</p>
+                                                    <p class="description"glot-model="ishiwataD">音楽CD流通会社、芸能事務所を経て弁理士へ。独立後、縁あって株式会社ホットリンクの監査役に就任し、現在はCEO特別補佐を務める。中央集権に頼らない本人認証を可能にするブロックチェーン技術に着目し、さらに日本を始め世界でもまだ十分とはいえない多様性に対する不寛容に問題意識を持ち「famiee」の起ち上げから関わる。OneMile商標知的財産事務所。Arts and Lawメンバー。一般社団法人Thinking Entertainments共同代表</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -349,9 +349,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/kawa.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">川大揮</h3>
-                                                    <p class="small-text">withID株式会社CEO</p>
-                                                    <p class="description">東京工業大学大学院在学中からブロックチェーンに可能性を感じブロックチェーンに関わる学生団体の代表やイスラエルのスタートアップBancorの日本代表を通して、日本でのブロックチェーンの普及活動を行う。最近は、ブロックチェーンを用いた権利証明や本人確認の仕組みに可能性を感じ、本人確認プラットホームであるwithIDというIDのプラットホームを立ち上げた。開発メンバー。</p>
+                                                    <h3 class="title" glot-model="kawa">川大揮</h3>
+                                                    <p class="small-text" glot-model="withidK">withID株式会社CEO</p>
+                                                    <p class="description" glot-model="kawaD">東京工業大学大学院在学中からブロックチェーンに可能性を感じブロックチェーンに関わる学生団体の代表やイスラエルのスタートアップBancorの日本代表を通して、日本でのブロックチェーンの普及活動を行う。最近は、ブロックチェーンを用いた権利証明や本人確認の仕組みに可能性を感じ、本人確認プラットホームであるwithIDというIDのプラットホームを立ち上げた。開発メンバー。</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -364,9 +364,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/sota.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">渡辺創太</h3>
-                                                    <p class="small-text">Staked株式会社 CEO</p>
-                                                    <p class="description">インド、中国、ロシアでのNPO活動を経て、2016年にブロックチェーンの可能性を感じ、シリコンバレーに渡航、現地のスタートアップであるChronicledでインターンシップを経て就職。帰国後、Staked株式会社を創業。ブロックチェーンのプロトコル開発を行うと同時に啓蒙活動を行っている。東京大学大学院工学部ブロックチェーン共同研究員。開発メンバー。</p>
+                                                    <h3 class="title" glot-model="watanabe">渡辺創太</h3>
+                                                    <p class="small-text" glot-model="stakeD">Stake株式会社 CEO</p>
+                                                    <p class="description" glot-model="watanabeD">インド、中国、ロシアでのNPO活動を経て、2016年にブロックチェーンの可能性を感じ、シリコンバレーに渡航、現地のスタートアップであるChronicledでインターンシップを経て就職。帰国後、Staked株式会社を創業。ブロックチェーンのプロトコル開発を行うと同時に啓蒙活動を行っている。東京大学大学院工学部ブロックチェーン共同研究員。開発メンバー。</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -378,9 +378,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/nice.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">村上乃須</h3>
-                                                    <p class="small-text">株式会社ホットリンク元役員秘書</p>
-                                                    <p class="description">社会課題を解決するための仕組みづくりに強い関心を持ち、「たくさんの家族の形が当たり前に認められ、誰もが家族を持つことができる社会を実現する」というアプローチに共感し、「famiee」の起ち上げに参画。<br/><br/><br/><br/><br/></p>
+                                                    <h3 class="title" glot-model="nice">村上乃須</h3>
+                                                    <p class="small-text" glot-model="hottolinkKKK">株式会社ホットリンク元役員秘書</p>
+                                                    <p class="description"glot-model="niceD">社会課題を解決するための仕組みづくりに強い関心を持ち、「たくさんの家族の形が当たり前に認められ、誰もが家族を持つことができる社会を実現する」というアプローチに共感し、「famiee」の起ち上げに参画。<br/><br/><br/><br/><br/></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -392,9 +392,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/fumino.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">杉山文野</h3>
-                                                    <p class="small-text">NPO法人東京レインボープライド共同代表理事</p>
-                                                    <p class="description">フェンシング元女子日本代表。
+                                                    <h3 class="title" glot-model="sugiyama">杉山文野</h3>
+                                                    <p class="small-text" glot-model="NPO2">NPO法人東京レインボープライド共同代表理事</p>
+                                                    <p class="description" glot-model="sugiyamaD">フェンシング元女子日本代表。
                                                         早稲田大学大学院にてセクシュアリティを研究後、トランスジェンダーである自身の体験を織り交ぜた『ダブルハッピネス』を講談社より出版、話題をよんだ。
                                                         日本初となる渋谷区・同性パートナーシップ条例制定に関わり、現在は渋谷区男女平等・多様性社会推進会議委員も務める。<br/><br/><br/><br/></p>
                                                 </div>
@@ -408,9 +408,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/okajima.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">岡島悦子</h3>
-                                                    <p class="small-text">株式会社プロノバ 代表取締役社長</p>
-                                                    <p class="description">三菱商事、Harvard Business School(MBA)、マッキンゼー、グロービスの経営陣を経て、2006年に経営チーム強化コンサルティングのプロノバを設立。丸井グループ等、上場企業５社未上場３社の社外取締役を務める。2018年50代で1児の母となったことから多様な働き方や家族のあり方についても啓蒙活動、社会課題解決への貢献を行なっている。<br/><br/><br/><br/></p>
+                                                    <h3 class="title" glot-model="okajima">岡島悦子</h3>
+                                                    <p class="small-text" glot-model="puronomaK">株式会社プロノバ 代表取締役社長</p>
+                                                    <p class="description" glot-model="okajimaD">三菱商事、Harvard Business School(MBA)、マッキンゼー、グロービスの経営陣を経て、2006年に経営チーム強化コンサルティングのプロノバを設立。丸井グループ等、上場企業５社未上場３社の社外取締役を務める。2018年50代で1児の母となったことから多様な働き方や家族のあり方についても啓蒙活動、社会課題解決への貢献を行なっている。<br/><br/><br/><br/></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -422,9 +422,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/fukushima.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">福島良典</h3>
-                                                    <p class="small-text">株式会社LayerXの代表取締役社長</p>
-                                                    <p class="description">大学院在学中に『Gunosy（グノシー）』のサービスを開発し、2012年11月に株式会社Gunosyを創業、代表取締役に就任後、2013年11月代表取締役最高経営責任者に就任。同社は創業より約2年半というスピードで東証マザーズに上場、2017年12月には東証第一部へ市場変更する。2018年8月よりブロックチェーン領域の技術開発のために新たに設立した、株式会社LayerXの代表取締役社長に就任。2016年には『Forbes Asia』より『アジアを代表する「30歳未満」』に選出される。</p>
+                                                    <h3 class="title"glot-model="fukushima" >福島良典</h3>
+                                                    <p class="small-text" glot-model="LayerXD">株式会社LayerXの代表取締役社長</p>
+                                                    <p class="description" glot-model="fukushimaD">大学院在学中に『Gunosy（グノシー）』のサービスを開発し、2012年11月に株式会社Gunosyを創業、代表取締役に就任後、2013年11月代表取締役最高経営責任者に就任。同社は創業より約2年半というスピードで東証マザーズに上場、2017年12月には東証第一部へ市場変更する。2018年8月よりブロックチェーン領域の技術開発のために新たに設立した、株式会社LayerXの代表取締役社長に就任。2016年には『Forbes Asia』より『アジアを代表する「30歳未満」』に選出される。</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -437,9 +437,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/mai.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">藤本真衣</h3>
+                                                    <h3 class="title" glot-model="mai">藤本真衣</h3>
                                                     <p class="small-text">Miss Bitcoin</p>
-                                                    <p class="description">ブロックチェーン分野における世界的な専門家とも親交が深く、“MissBitcoin”と呼ばれ親しまれている。 CryptoWeeklyの「THE 100 MOST INFLUENTIAL PEOPLE IN CRYPTO」に選出された。 ブロックチェーン業界に特化したPR会社「Gracone」をはじめ、日本初の暗号通貨による寄付サイト「KIZUNA」ブロックチェーン業界に特化した就職・転職支援会社「withB」を立ち上げる。</p>
+                                                    <p class="description" glot-model="maiD">ブロックチェーン分野における世界的な専門家とも親交が深く、“MissBitcoin”と呼ばれ親しまれている。 CryptoWeeklyの「THE 100 MOST INFLUENTIAL PEOPLE IN CRYPTO」に選出された。 ブロックチェーン業界に特化したPR会社「Gracone」をはじめ、日本初の暗号通貨による寄付サイト「KIZUNA」ブロックチェーン業界に特化した就職・転職支援会社「withB」を立ち上げる。</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -452,9 +452,9 @@
                                                     <img alt="..." class="img-circle" src="assets/img/faces/竹中平蔵.jpg"/>
                                                 </div>
                                                 <div class="description">
-                                                    <h3 class="title">竹中平蔵</h3>
-                                                    <p class="small-text">特別アドバイザー / 慶應義塾大学名誉教授</p>
-                                                    <p class="description">博士（経済学）。ハーバード大学客員准教授、慶應義塾大学総合政策学部教授などを経て、2001年小泉内閣で経済財政政策担当大臣を皮切りに、金融担当大臣、郵政民営化担当大臣兼務、総務大臣を歴任。東洋大学教授、慶應義塾大学名誉教授、アカデミーヒルズ理事長等を兼務。プロジェクト特別アドバイイザー。<br/><br/></p>
+                                                    <h3 class="title" glot-model="take">竹中平蔵</h3>
+                                                    <p class="small-text" glot-model="keio">特別アドバイザー / 慶應義塾大学名誉教授</p>
+                                                    <p class="description" glot-model="takeD">博士（経済学）。ハーバード大学客員准教授、慶應義塾大学総合政策学部教授などを経て、2001年小泉内閣で経済財政政策担当大臣を皮切りに、金融担当大臣、郵政民営化担当大臣兼務、総務大臣を歴任。東洋大学教授、慶應義塾大学名誉教授、アカデミーヒルズ理事長等を兼務。プロジェクト特別アドバイイザー。<br/><br/></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -466,7 +466,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>    
 
     <div class="section section-small section-get-started">
         <div class="parallax filter">
@@ -488,6 +488,7 @@
         </div>
     </div>
 
+
     <footer class="footer footer-big footer-color-black" data-color="black">
         <div class="container">
             <div class="row">
@@ -497,10 +498,7 @@
                         <nav>
                             <ul>
                                 <li>
-                                    <a href="https://www.famiee.com">Home</a>
-                                </li>
-                                <li>
-                                    <a href="./PrivacyPolicy.html">Privacy Policy</a>
+                                    <a href="#">Home</a>
                                 </li>
                             </ul>
                         </nav>
@@ -512,7 +510,7 @@
                          <nav>
                             <ul>
                                 <li>
-                                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdkd6T2Gp3D9m0NXIYa4utQ7_cxBVkTNLZsO7O-oEeWKMiGRg/viewform">Contact Us</a>
+                                    <a href="#">Contact Us</a>
                                 </li>
                             </ul>
                         </nav>
@@ -521,13 +519,6 @@
                 <div class="col-md-3 col-sm-3">
                     <div class="info">
                         <h5 class="title">Latest News</h5>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="https://www.famiee.com#news">News List</a>
-                                </li>
-                            </ul>
-                        </nav>
                     </div>
                 </div>
                 <div class="col-md-2 col-md-offset-1 col-sm-3">
@@ -536,11 +527,10 @@
                         <nav>
                             <ul>
                                 <li>
-                                    <a href="https://twitter.com/FamieeP"  class="btn btn-social btn-twitter btn-simple">
+                                    <a href="https://twitter.com/FamieeP" class="btn btn-social btn-twitter btn-simple">
                                         <i class="fa fa-twitter"></i> Twitter
                                     </a>
                                 </li>
-                                <a class="twitter-timeline" data-lang="ja" data-width="350" data-height="300" data-theme="light" data-link-color="#981CEB" href="https://twitter.com/FamieeP?ref_src=twsrc%5Etfw">Tweets by FamieeP</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                             </ul>
                         </nav>
                     </div>
@@ -561,12 +551,12 @@ const glot = new Glottologist();
 //glot-modelの属性値が「title」の文章を翻訳
 
 
-    glot.assign('metadate', {
+glot.assign('metadate', {
         ja: '「Famiee」プロジェクトは、LGBTカップルや事実婚カップル等、法的には婚姻関係と認められない多様な家族形態の人たちが、住んでいる地区に関わらず家族関係を証明することができるよう、ブロックチェーン技術を用いたパートナーシップ証明書の発行を目指します。さらに、多様な家族形態の人たちが等しく民間企業の家族向けサービスを受けることができるよう、パートナーシップ証明書を採用する民間企業を増やすための啓蒙活動を行い、これらの活動によって多様な家族形態が当たり前のように認められる社会の実現を目指します。',
         en: 'Famiee Project aims to issue a certificate of a same sex partnership using the blockchain system. Our goal is to enable anyone who cannot be recognized as married legally including LGBTQ+ couples to be accepted as a married couple, regardless of where you live. This is because anyone within diverse family forms should be able to receive private survices equally.',
     })
     
-    glot.assign('mission', {
+glot.assign('mission', {
     
         ja: '多様な家族形態が当たり前のように認められる社会の実現のために',
         en: 'For the realization of a society where various family forms are recognized as a matter of course',
